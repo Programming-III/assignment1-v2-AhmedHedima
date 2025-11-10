f483216291da9675fb5184f3537f968309d40f8e
AhmedHedima <127694108+AhmedHedima@users.noreply.github.com>
2025-11-10 22:16:54 +0200

    Add Person class with display functionality
    Implemented the Person class with constructors and display method.

--- Person.h
+++ Person.h
@@ -1,20 +1,26 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <iostream>
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+protected:
+    string name;
+    int id;
 
+public:
+    Person() {}
+    Person(string name, int id) : name(name), id(id) {}
 
+    virtual void display() const {
+        cout << "Name: " << name << " (ID: " << id << ")" << endl;
+    }
 
-
-
-
-
-
-
-
-
+    // Getters (optional, useful later)
+    string getName() const { return name; }
+    int getId() const { return id; }
+};
 
 #endif
