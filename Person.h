4acb1ddc8b7e573e8dbd509c006d2d543cbd9189
AhmedHedima <127694108+AhmedHedima@users.noreply.github.com>
2025-11-10 22:17:38 +0200

    Refactor Person.h by removing class definition
    Removed the Person class implementation and included a placeholder for future code.

--- Person.h
+++ Person.h
@@ -1,26 +1,20 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#include <iostream>
 #include <string>
 using namespace std;
 
-class Person {
-protected:
-    string name;
-    int id;
+//#write Person class here
 
-public:
-    Person() {}
-    Person(string name, int id) : name(name), id(id) {}
 
-    virtual void display() const {
-        cout << "Name: " << name << " (ID: " << id << ")" << endl;
-    }
 
-    // Getters (optional, useful later)
-    string getName() const { return name; }
-    int getId() const { return id; }
-};
+
+
+
+
+
+
+
+
 
 #endif
